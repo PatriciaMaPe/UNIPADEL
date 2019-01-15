2713bab7c0a75f0057ae39f9155738a3af78a2ca
Patricia <martinperez.patricia@gmail.com>
2019-01-15 23:51:29 +0100

    Agregado action en boton volver

--- view/reservas/reserva.php
+++ view/reservas/reserva.php
@@ -10,14 +10,14 @@ $view->setVariable("title", "Gestionar reservas");
 	<section>
 
 					<table class="table" >
-				
+
 							<thead><tr><th>Datos<th></tr></thead>
-								
+
 								<form method="POST" action="index.php?controller=RealizarReserva&amp;action=anadirReserva">
 									<tbody>
-									
+
 										<tr><th>Usuario</th><td> <input style="border:none" type="text" name="usuario" value="<?= $_SESSION["currentuser"];?>" readonly></td></tr>
-									
+
 									<tr><th>Pista</th><td> <input style="border:none" type="text" name="pista" value="<?= $view->getVariable("pista");?>" readonly></td></tr>
 									<tr><th>Hora Inicial</th><td> <input style="border:none" type="text" name="hora" value="<?= $view->getVariable("hora");?>" readonly></td></tr>
 									<tr><th>Hora Final</th><td> <input style="border:none" type="text" name="horaFinal" value="<?= $view->getVariable("horaFinal");?>:00" readonly></td></tr>
@@ -26,38 +26,38 @@ $view->setVariable("title", "Gestionar reservas");
 										$fecha = $view->getVariable("fecha");
 										$nuevafecha = strtotime ( '-7 day' , strtotime ( $fecha ) ) ;
 										$nuevafecha = date ( 'Y/m/d' , $nuevafecha );
-										
+
 									?>
 									<tr><th>Fin inscripciones</th><td> <input style="border:none" type="text" name="finInscripcion" <?php echo "value='".$nuevafecha."'";?> readonly></td></tr>
-									
+
 									<tr><th>Numero Inscritos</th><td> <input style="border:none" type="number" name="numInscritos" value="<?= $view->getVariable("numInscritos");?>" readonly></td></tr>
-									
-									
-								
+
+
+
 									<tr><th></th><td> <input style="border:none" type="hidden" name="disponibilidad" value="<?= $view->getVariable("disponibilidad");?>" readonly></td></tr>
 
 
-									
-									
-									
+
+
+
 									</tbody></table>
 
-									<a href="index.php?controller=RealizarReserva"><button type="button" >Volver</button></a>
+									<a href="index.php?controller=RealizarReserva&amp;action=index"><button type="button" >Volver</button></a>
 											<input type="submit" name="inscripcion" value="Inscribirse" >
 											<input type="submit" name="desinscribirse" value="Cancelar Inscripcion" >
-											
+
 											<input type="submit" name="reservar" value="Reservar" >
-										
-									
-									
-									
-									
+
+
+
+
+
 								</form>
 								<td></tr></tbody>
-							
+
 					</table>
-		
-							
-					
-				
+
+
+
+
 	</section>
