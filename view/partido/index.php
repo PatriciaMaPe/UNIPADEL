5e7c8624643a087913c6aa1fcf79e8e21b4a4817
Nacho <rodriguezpereiranacho@gmail.com>
2019-01-16 22:43:50 +0100

    Vista azul en partidos

--- view/partido/index.php
+++ view/partido/index.php
@@ -39,17 +39,17 @@ $currenttype = $_SESSION["currenttype"];
                             $anterior = $pista->getHorarioIdPista();
                         }
                         ?>
-                        <?php if ($pista->getDisponibilidad() == 'disponible') { 
+                        <?php if ($pista->getDisponibilidad() != 'disponible') { 
                                 if($pista->getNumInscritos()=='' ||$pista->getNumInscritos()=='0'){?>
-                                    <td><a style="border:solid;background-color:green;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
+                                    <td><a style="border:solid;background-color:blue;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
                                     <?php
                                 }else{
                                     ?>
-                                    <td><a style="border:solid;background-color:blue;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
+                                    <td><a style="border:solid;background-color:red;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
                                     <?php
                                 }
                          } else { ?>
-                            <td><a style="border:solid;background-color:red;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
+                            <td><a style="border:solid;background-color:green;color:black;" href="index.php?controller=Partido&amp;action=comprobarReservaPartido&amp;fecha=<?= $pista->getFecha(); ?>&amp;pista=<?= $pista->getHorarioIdPista(); ?>&amp;hora=<?= $pista->getHora(); ?>&amp;disponibilidad=<?= $pista->getDisponibilidad(); ?>"><?php echo $pista->getHora(); ?></a></td>
         <?php } ?>
     <?php }
 } ?>
